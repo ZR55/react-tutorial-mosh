efe85b4400c000531d64bd67d72170df2ebbb4b2
Rui Zhang <zhangduanrui57@gmail.com>
2025-02-12 14:33:11 -0500

    styling with classes - challenge1

--- react-scrimba/src/App.jsx
+++ react-scrimba/src/App.jsx
@@ -9,7 +9,13 @@ function Header() {
   return (
       <header>
           <img src="./public/react-logo.png" width="40px" alt="React logo" />
-          
+          <nav>
+            <ul className="nav-list">
+              <li>Pricing</li>
+              <li>About</li>
+              <li>Contact</li>
+            </ul>
+          </nav>
       </header>
   )
 }
