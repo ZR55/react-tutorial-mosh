fe5c8abfb48556b14de96d95c361512705e6c810
Rui Zhang <zhangduanrui57@gmail.com>
2025-02-17 13:43:12 -0500

    ReactFacts Project - Main Content Section - challenge 1 - answer

--- react-scrimba/src/components/Main.jsx
+++ react-scrimba/src/components/Main.jsx
@@ -1,8 +1,16 @@
+/**
+ * Challenge: Build out the main content section!
+ *
+ * For now, skip 2 aspects of the design and we'll work on them later:
+ *   1. The colored bullets in the list
+ *   2. The larger gray React logo on the side
+ */
+
 export default function Main() {
   return (
     <main>
       <h1>Fun facts about React</h1>
-      <ul>
+      <ul className="facts-list">
         <li>Was first released in 2013</li>
         <li>Was originally created by Jordan Walke</li>
         <li>Has well over 200K stars on GitHub</li>
