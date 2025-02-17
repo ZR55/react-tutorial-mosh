7b68689c3477732186f9b47c56134ee73145136a
Rui Zhang <zhangduanrui57@gmail.com>
2025-02-17 11:06:52 -0500

    ReactFacts Project - Navbar styling - challenge 1 - submit

--- react-scrimba/src/components/Navbar.jsx
+++ react-scrimba/src/components/Navbar.jsx
@@ -1,8 +1,24 @@
-export default function Header() {
-    return (
-        <header className="header">
-            <img className="nav-logo" src="./public/react-logo.png" alt="React logo" />
-            <h1>Navbar goes here</h1>
-        </header>
-    )
-  }
+/**
+ * Challenge: complete the Navbar to match the Figma design
+ *
+ * Hints:
+ * - for semantic HTML purposes, the Navbar should render
+ *   a <header> with a <nav> nested inside. The image and "ReactFacts"
+ *   text elements can both be rendered as children inside the <nav>
+ * - reference the Figma design for the most accurate info about
+ *   colors, sizes, font information, etc.
+ */
+
+export default function Navbar() {
+  return (
+    <header className="header inter-400">
+      <img
+        className="nav-logo"
+        src="./public/react-logo.png"
+        alt="React logo"
+      />
+      <h1 className="nav-title">ReactFacts</h1>
+      <nav></nav>
+    </header>
+  );
+}
